671c14331ed1027a7d2dae459d1cc1288115059a
Santiago <santiagof00@gmail.com>
2022-07-31 10:45:42 -0300

    agrego resolucion ejercicio 2 de funciones

---
+++ ejercicio2(F).js
@@ -0,0 +1,15 @@
+let verifCadena = (cadena) => {
+  let resultado = 'La cadena "' + cadena + '" ';
+
+  if (cadena == cadena.toUpperCase()) {
+    resultado += " está formada sólo por mayúsculas";
+  } else if (cadena == cadena.toLowerCase()) {
+    resultado += " está formada sólo por minúsculas";
+  } else {
+    resultado += " está formada por mayúsculas y minúsculas";
+  }
+
+  return resultado;
+};
+
+document.write(verifCadena(prompt("Ingrese una cadena de texto")));
